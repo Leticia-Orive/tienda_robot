73c9b7fcf3f7045594cd1d85527084f9d41e002e
Leticia Orive Peña <lorivepe@ext.unicaja.es>
2024-05-07 15:13:13 +0200

    Creaccion de metodo y interfaz

--- joes-robot-shop/src/app/home/home.component.ts
+++ joes-robot-shop/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+/**Primero importamos la interfaz en este caso OnInit y lo colocamos en su clase*/
 @Component({
   selector: 'bot-home', // Cambio app por bot porque sino me funciona porque tambien lo cambie en angular json.
   /** Poner el html y el css de otra forma distinta dentro de este archivo
@@ -12,4 +12,16 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit{
+  /**Aqui */
+/**Creamos el constructor */
+constructor(){
+
+}
+/** Creamos el metodo en la clase, en este metodo es donde se pone cualquier logica que quieras
+ * Se ejecuta cuando se activa el gancho del ciclo de la vida
+ */
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+} 
