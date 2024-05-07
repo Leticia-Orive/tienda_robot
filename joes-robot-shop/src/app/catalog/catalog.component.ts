12608425e613245bf0e8473c12bda7fc500ef89f
Leticia Orive Peña <lorivepe@ext.unicaja.es>
2024-05-07 16:14:53 +0200

    Componentes con interpolación

--- joes-robot-shop/src/app/catalog/catalog.component.ts
+++ joes-robot-shop/src/app/catalog/catalog.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IProduct } from './product.model';
 
 @Component({
   selector: 'bot-catalog',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./catalog.component.css']
 })
 export class CatalogComponent {
+  /**Definir una propiedad de producto y los tipo de datos que necesito */
+  /**Las propiedades de las clase  pero si lo quiero hacer privado tendria que poner private delante*/
+  product: IProduct;
 
+/**Creacción del constructor para la clase */
+constructor(){
+  /*Iniciar la propiedad del nuevo producto y lo configure en un nuevo objeto que se vea asi*/ 
+  this.product = {
+    id: 2,
+    description:"A friendly robot head with two eyes and a smile -- great for domestic use.",
+    name: "Friendly Bot",
+    imageName: "head-friendy.png",
+    category: "Heads",
+    price: 945.0,
+    discount: 0.2,
+  };
+
+}
 }
