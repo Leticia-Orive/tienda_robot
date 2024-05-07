7fb4f04658229f0850030386e7dd647ef76f7d1d
Leticia Orive Peña <lorivepe@ext.unicaja.es>
2024-05-07 13:40:00 +0200

    forma de poner el html y css dentro del archivo ts

--- joes-robot-shop/src/app/home/home.component.ts
+++ joes-robot-shop/src/app/home/home.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'bot-home',// Cambio app por bot porque sino me funciona porque tambien lo cambie en angular json.
+  selector: 'bot-home', // Cambio app por bot porque sino me funciona porque tambien lo cambie en angular json.
+  /** Poner el html y el css de otra forma distinta dentro de este archivo
+   * html
+   * templete:'./home.component.html',
+   * templete:`<p class="red">Inline home words!</p>´
+   * CSS
+   * style: [`.red { color: red;}´],
+   */
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
