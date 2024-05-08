01e572f405f40c51a642a314755abf5212d0c7d7
Leticia Orive Peña <lorivepe@ext.unicaja.es>
2024-05-08 09:04:46 +0200

    Comunicación con componentes principales mediante salida

--- joes-robot-shop/src/app/product-details/product-details.component.ts
+++ joes-robot-shop/src/app/product-details/product-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { IProduct } from '../catalog/product.model';
 
 @Component({
@@ -9,14 +9,14 @@ import { IProduct } from '../catalog/product.model';
 export class ProductDetailsComponent {
   /**Voy a hacer un @Input con un abierto y un cerrado paren, tenemos que importar esa entrada */
   @Input() product!: IProduct;
+  /**Agregar una propiedad analoga llamada propiedad de salida */
+  @Output() buy = new EventEmitter();
   /*product!: IProduct;*/
   getImageUrl(product: IProduct) {
     if (!product) return '';
     return '/assets/images/robot-parts/' + product.imageName;
   }
-  addToCart(product: IProduct) {}
-  getDiscountedClasses(product: IProduct) {
-    if (product.discount > 0) return ['strikethrogh bold'];
-    else return [];
+  buyButtonClicked(product: IProduct) {
+    this.buy.emit();
   }
 }
